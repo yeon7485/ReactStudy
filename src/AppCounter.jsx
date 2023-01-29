39237f1991dbcf54e0b36f30420087f71c03692f
박세연 <wsx4379@naver.com>
2023-01-29 23:04:12 +0900

    #4.16

---
+++ src/AppCounter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+export default function AppCounter() {
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
+}
+
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0); 
+  return (
+    <div className="counter">
+      <span className="number">{count}</span>
+      <button
+        className="button"
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Add +
+      </button>
+    </div>
+  );
+}
