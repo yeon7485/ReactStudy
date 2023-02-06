55d05348d5c232ebe4eb6002cc1a8d74645a623a
박세연 <wsx4379@naver.com>
2023-02-06 23:00:49 +0900

    #4.18

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 
-export default function Counter() {
-  const [count, setCount] = useState(0); 
+export default function Counter({ total, onClick }) {
+  const [count, setCount] = useState(0);
   return (
     <div className="counter">
-      <span className="number">{count}</span>
+      <span className="number">{count} <span className="total">/{total}</span></span>
       <button
         className="button"
         onClick={() => {
           setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
-          setCount((prev) => prev + 1);
+          onClick();
         }}
       >
         Add +
