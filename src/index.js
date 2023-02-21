9e771f141932989bf272af82b340b7b3903a9ea5
박세연 <wsx4379@naver.com>
2023-02-21 19:49:14 +0900

    #5.13

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppProfile from "./AppProfile";
 import reportWebVitals from "./reportWebVitals";
-import AppCounter from "./AppCounter";
-import AppProducts from "./AppProducts";
+import AppXY from "./AppXY";
+import AppMentor from "./AppMentor";
+import AppMentors from "./AppMentors";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AppProducts />
+        <AppMentors />
     </React.StrictMode>
 );
 
