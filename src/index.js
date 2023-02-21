7be6af8e95d5bd3801c102147c99e282df73f44f
박세연 <wsx4379@naver.com>
2023-02-21 23:44:57 +0900

    #5.14

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import reportWebVitals from "./reportWebVitals";
 import AppXY from "./AppXY";
 import AppMentor from "./AppMentor";
 import AppMentors from "./AppMentors";
+import AppForm from "./AppForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AppMentors />
+        <AppForm />
     </React.StrictMode>
 );
 
